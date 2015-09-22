6a6bd2dd276fc5aa7fa0c64d6e855ab3a6fedda1
Sam Hain <hain.sa@gmail.com>
2015-09-22 08:51:27 -0700

    Further changes to clean up code per specifications in python style guide and recommendations via code reviewer and PEP8 checker.

--- media.py
+++ media.py
@@ -1,17 +1,18 @@
 import webbrowser
 
-class Movie():
-	# defines instance of movie with appropriate attributes
-	def __init__(self, movie_title, movie_year, movie_storyline, poster_image,
-								trailer_youtube, movie_director, movie_rating=1):
-		self.title = movie_title
-		self.year = movie_year
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		self.director = movie_director
-		self.rating = movie_rating
 
-	# method to open youtube url in browser window
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+class Movie():
+    # defines instance of movie with appropriate attributes
+    def __init__(self, movie_title, movie_year, movie_storyline, poster_image,
+                 trailer_youtube, movie_director, movie_rating=1):
+        self.title = movie_title
+        self.year = movie_year
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.director = movie_director
+        self.rating = movie_rating
+
+    # method to open youtube url in browser window
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
